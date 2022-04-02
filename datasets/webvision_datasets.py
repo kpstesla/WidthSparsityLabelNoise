1982bf2d7f0f264565d724cc91a3d047dc0f62f8
kpstesla <kswhitecross@gmail.com>
2022-04-02 14:06:46 -0700

    add webvision dataset

--- datasets/webvision_datasets.py
+++ datasets/webvision_datasets.py
@@ -63,3 +63,70 @@ class ImagenetDataset(Dataset):
             label = self.target_transform(label)
 
         return x, label
+
+
+class WebvisionDataset(Dataset):
+    def __init__(self, data_root, num_classes=50, train=True, include_flickr=False, transform=None,
+                 target_transform=None):
+        """
+        Webvision Dataset.  Root dir must be organized as
+
+        root/
+         - info/
+          - val_filelist.txt
+          - train_filelist_google.txt
+          - train_filelist_flickr.txt
+         - val_images_256/
+         - google/
+         - flickr/
+
+        :param data_root: filepath of data root
+        :param num_classes: number of classes to use
+        :param train: true for training data, false for validation data
+        :param include_flickr: true to include flickr examples, false to exclude them
+        :param transform: transform to apply to images
+        :param target_transform: transform to apply to labels
+        """
+        self.data_root = data_root
+        self.num_classes = num_classes
+        self.transform = transform
+        self.target_transform = target_transform
+        self.img_paths = []
+        self.labels = []
+
+        # get path class mappings
+        if train:
+            with open(os.path.join(self.data_root, 'info', 'train_filelist_google.txt')) as f:
+                lines = f.readlines()
+            if include_flickr:
+                with open(os.path.join(self.data_root, 'info', 'train_filelist_flickr.txt')) as f:
+                    lines += f.readlines()
+        else:
+            with open(os.path.join(self.data_root, 'info', 'val_filelist.txt')) as f:
+                lines = f.readlines()
+
+        # populate img_paths and labels
+        for line in lines:
+            s = line.split()
+            path, label = s[0], int(s[1])
+            if not train:
+                path = os.path.join('val_images_256/', path)
+
+            path = os.path.join(self.data_root, path)
+
+            self.img_paths.append(path)
+            self.labels.append(s[1])
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, ind):
+        path, label = self.img_paths[ind], self.labels[ind]
+        img = Image.open(path).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            label = self.target_transform(label)
+
+        return img, label
