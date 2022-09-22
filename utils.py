dba3b20cf760cb36c427196e6441846af7b47248
kpstesla <kswhitecross@gmail.com>
2022-09-22 11:33:01 -0700

    bug

--- utils.py
+++ utils.py
@@ -121,8 +121,8 @@ def load_datasets(args):
 
     # train data augmentation on the fly
     caltech_train = transforms.Compose([
-        transforms.Resize(384),
-        transforms.RandomCrop((299, 299)),
+        transforms.Resize(324),
+        transforms.RandomCrop((224, 224)),
         transforms.RandomHorizontalFlip(),
         transforms.Lambda(enhance),
         transforms.ToTensor(),
@@ -134,7 +134,7 @@ def load_datasets(args):
 
     # validation data is already resized
     caltech_val = transforms.Compose([
-        transforms.Resize((299, 299)),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406],
