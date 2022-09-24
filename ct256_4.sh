cc5f9d9c5fc57b2c42211b99d9f1dbb658e594ad
kpstesla <kswhitecross@gmail.com>
2022-09-24 11:17:33 -0700

    add dp

--- ct256_4.sh
+++ ct256_4.sh
@@ -5,6 +5,6 @@ python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --su
 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.2 --width 4 --run_name "ct256_r18_n60_s020_w4" --gpu 4
 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.2 --width 256 --run_name "ct256_r18_n60_s020_w256" --gpu 4
 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.5 --width 4 --run_name "ct256_r18_n60_s050_w4" --gpu 4
-python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.5 --width 256 --run_name "ct256_r18_n60_s050_w256" --gpu 4
+CUDA_VISIBLE_DEVICES=4,5,6,7 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.5 --width 256 --run_name "ct256_r18_n60_s050_w256" --gpu 4 --dataparallel
 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 1.0 --width 4 --run_name "ct256_r18_n60_s100_w4" --gpu 4
-python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 1.0 --width 256 --run_name "ct256_r18_n60_s100_w256" --gpu 4
+CUDA_VISIBLE_DEVICES=4,5,6,7 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 1.0 --width 256 --run_name "ct256_r18_n60_s100_w256" --gpu 4 --dataparallel
--- ct256_5.sh
+++ ct256_5.sh
@@ -5,6 +5,6 @@ python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --su
 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.2 --width 8 --run_name "ct256_r18_n60_s020_w8" --gpu 5
 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.2 --width 192 --run_name "ct256_r18_n60_s020_w192" --gpu 5
 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.5 --width 8 --run_name "ct256_r18_n60_s050_w8" --gpu 5
-python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.5 --width 192 --run_name "ct256_r18_n60_s050_w192" --gpu 5
+CUDA_VISIBLE_DEVICES=4,5,6,7 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 0.5 --width 192 --run_name "ct256_r18_n60_s050_w192" --gpu 5 --dataparallel
 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 1.0 --width 8 --run_name "ct256_r18_n60_s100_w8" --gpu 5
-python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 1.0 --width 192 --run_name "ct256_r18_n60_s100_w192" --gpu 5
+CUDA_VISIBLE_DEVICES=4,5,6,7 python main.py --config exps/caltech_r18.yaml --mislabel_ratio 0.4 --subset --subset_size 1.0 --width 192 --run_name "ct256_r18_n60_s100_w192" --gpu 5 --dataparallel
